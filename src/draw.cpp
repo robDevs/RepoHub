a617904a49bd21202556293e5a46d27cbcd2d1cd
robDevs <robbdevv@gmail.com>
2019-07-19 17:04:20 -0600

    update draw func's

--- src/draw.cpp
+++ src/draw.cpp
@@ -32,3 +32,18 @@ void draw_list_item(std::string name, int y, bool selected) {
         vita2d_draw_line(0, y + 100, 960, y + 100, RGBA8(150,150,150,150));
     }
 }
+
+void draw_list_item_small(std::string name, int y, bool selected) {
+    vita2d_draw_rectangle(0, y, 960, 100, RGBA8(255,255,255,255));
+    vita2d_font_draw_textf(font20, 40, y+50+(vita2d_font_text_height(font20, 20.0f, name.c_str()) / 4), RGBA8(0,0,0,255), 20.0f, "%s", name.c_str());
+    if(selected) {
+        vita2d_draw_line(0, y, 960, y, RGBA8(36,41,46,255));
+        vita2d_draw_line(0, y+1, 960, y+1, RGBA8(36,41,46,255));
+        vita2d_draw_line(0, y + 100, 960, y + 100, RGBA8(36,41,46,255));
+        vita2d_draw_line(0, y + 100 - 1, 960, y + 100 - 1, RGBA8(36,41,46,255));
+    }
+    else {
+        vita2d_draw_line(0, y, 960, y, RGBA8(150,150,150,150));
+        vita2d_draw_line(0, y + 100, 960, y + 100, RGBA8(150,150,150,150));
+    }
+}
