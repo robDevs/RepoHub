5188320ce369669a6faae90c2b067da0ede16531
robDevs <robbdevv@gmail.com>
2019-07-20 01:26:24 -0600

    Add time to header

--- src/draw.cpp
+++ src/draw.cpp
@@ -1,8 +1,19 @@
 #include "draw.h"
+#include <psp2/kernel/processmgr.h>
+#include <psp2/rtc.h>
 
 void draw_header(std::string header) {
+    SceDateTime time;
+    sceRtcGetCurrentClockLocalTime(&time);
+
+    std::string time_string = "";
+    time_string += std::to_string(sceRtcGetHour(&time));
+    time_string += ":";
+    time_string += std::to_string(sceRtcGetMinute(&time));
     vita2d_draw_rectangle(0, 0, 960, 44, RGBA8(36,41,46,255));
     vita2d_font_draw_textf(font20, 20, 22+(vita2d_font_text_height(font20, 20.0f, header.c_str()) / 4), RGBA8(255,255,255,255), 20.0f, "%s", header.c_str());
+
+    vita2d_font_draw_textf(font20, 960-(vita2d_font_text_width(font20, 20.0f, time_string.c_str())) - 20, 22+(vita2d_font_text_height(font20, 20.0f, header.c_str()) / 4), RGBA8(255,255,255,255), 20.0f, "%s", time_string.c_str());
 }
 
 void draw_button(std::string name, int x, int y, int w, int h, bool selected) {
