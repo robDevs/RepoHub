b9348c593a0c57dd198c7ae04c70df4a38e17f04
robDevs <robbdevv@gmail.com>
2019-07-28 11:44:12 -0600

    give compile/send script command line args

--- magic.sh
+++ magic.sh
@@ -10,7 +10,10 @@ unzip repo-browser.vpk
 cp eboot.bin ../eboot.bin
 cd ../
 rm -r temp/
-echo destroy | nc 192.168.1.7 1338
-curl --ftp-method nocwd -T eboot.bin ftp://192.168.1.7:1337/ux0:/app/rbdv00002/
-echo launch rbdv00002 | nc 192.168.1.7 1338
 
+for i in $@
+do
+    echo destroy | nc $i 1338
+    curl --ftp-method nocwd -T eboot.bin ftp://$i:1337/ux0:/app/rbdv00002/
+    echo launch rbdv00002 | nc $i 1338
+done
