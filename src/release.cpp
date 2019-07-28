66ca8644f4d05253085767b9544602a1b934d1d1
robDevs <robbdevv@gmail.com>
2019-07-27 22:08:50 -0600

    added horiz scrolling to release notes

--- src/release.cpp
+++ src/release.cpp
@@ -8,10 +8,14 @@ void draw_release_details(Release release, std::string header_start) {
 
     int list_size = static_cast<int> (release.assets.size());
     int cursor_pos = 0;
+    int y_offset = 125;
 
     std::string final_body_string = release.body;
 
     strip_carriage_return(final_body_string);
+
+    int body_string_x = 40;
+    int body_string_y = 145;
 
     std::string header_string = header_start;
     header_string += "->";
@@ -52,6 +56,11 @@ void draw_release_details(Release release, std::string header_start) {
         if(cursor_pos < 0) cursor_pos = list_size -1;
         if(cursor_pos > list_size -1) cursor_pos = 0;
 
+        read_joy_sticks();
+
+        if(body_string_x + vita2d_font_text_width(font15, 15.0f, final_body_string.c_str()) + 40 > 960 - 40 && lx < 127 - 20) body_string_x -= 5;
+        if(body_string_x < 40 && lx > 127 + 20) body_string_x += 5;
+
         vita2d_start_drawing();
         vita2d_clear_screen();
 
@@ -71,7 +80,7 @@ void draw_release_details(Release release, std::string header_start) {
         }
         else {
             //body and body bg
-            vita2d_font_draw_text(font15, 40, 45+100, RGBA8(0,0,0,255), 15.0f, final_body_string.c_str());
+            vita2d_font_draw_text(font15, body_string_x, body_string_y, RGBA8(0,0,0,255), 15.0f, final_body_string.c_str());
 
             //sub-header bg.
             vita2d_draw_rectangle(0, 45, 960, 80, RGBA8(255,255,255,255));
