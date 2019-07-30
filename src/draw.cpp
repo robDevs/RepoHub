db8ad7e54d7f85e5601e1da2aded3842af773f6f
robDevs <robbdevv@gmail.com>
2019-07-30 00:20:53 -0600

    Center button text vertically

--- src/draw.cpp
+++ src/draw.cpp
@@ -36,12 +36,12 @@ void draw_button(std::string name, int x, int y, int w, int h, bool selected) {
     if(selected) {
         vita2d_draw_rectangle(x-2, y-2, w + 4, h+4, RGBA8(36,41,46,255));
         vita2d_draw_rectangle(x, y, w, h, RGBA8(255,255,255, 255));
-        vita2d_font_draw_textf(font20, x+w/2 - (vita2d_font_text_width(font20, 20.0f, name.c_str()) / 2), y + 25+vita2d_font_text_height(font20, 20.0f, name.c_str()) / 4, RGBA8(0,0,0,255), 20.0f, "%s", name.c_str());
+        vita2d_font_draw_textf(font20, x+w/2 - (vita2d_font_text_width(font20, 20.0f, name.c_str()) / 2), y + h/2+vita2d_font_text_height(font20, 20.0f, name.c_str()) / 4, RGBA8(0,0,0,255), 20.0f, "%s", name.c_str());
     }
     else {
         vita2d_draw_rectangle(x-1, y-1, w + 2, h + 2, RGBA8(150,150,150,150));
         vita2d_draw_rectangle(x, y, w, h, RGBA8(255,255,255, 255));
-        vita2d_font_draw_textf(font20, x+w/2 - (vita2d_font_text_width(font20, 20.0f, name.c_str()) / 2), y + 25+vita2d_font_text_height(font20, 20.0f, name.c_str()) / 4, RGBA8(150,150,150,150), 20.0f, "%s", name.c_str());
+        vita2d_font_draw_textf(font20, x+w/2 - (vita2d_font_text_width(font20, 20.0f, name.c_str()) / 2), y + h/2+vita2d_font_text_height(font20, 20.0f, name.c_str()) / 4, RGBA8(150,150,150,150), 20.0f, "%s", name.c_str());
     }
 }
 
