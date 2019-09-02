d4ab654f437476c65587e3f366905da9f3e7e0d9
robDevs <robbdevv@gmail.com>
2019-09-02 13:24:46 -0600

    button only template

--- src/draw_templates.cpp
+++ src/draw_templates.cpp
@@ -5,6 +5,8 @@ extern void drawTemplate(int type, std::string header, std::vector<Button> butto
         case SUB_HEADER_BUTTONS:
             subHeaderButtons(header, buttons, cursor_pos);
             break;
+        case BUTTONS_ONLY:
+            buttonsOnly(header, buttons, cursor_pos);
         default:
             break;
     }
@@ -18,6 +20,21 @@ extern void subHeaderButtons(std::string header, std::vector<Button> buttons, in
     //sub header area
     vita2d_draw_rectangle(0, 44, 960, 103-44, RGBA8(255,255,255,255));
     vita2d_draw_line(0, 103, 960, 103, RGBA8(150,150,150,200));
+
+    int num_buttons = static_cast<int>(buttons.size());
+
+    for(int i = 0; i < num_buttons; i++) {
+        draw_button(buttons[i], cursor_pos == i);
+    }
+
+    //draw the header
+    draw_header(header);
+}
+
+void buttonsOnly(std::string header, std::vector<button> buttons, int cursor_pos) {
+    //right hand menu bar
+    vita2d_draw_rectangle(960 - 260, 103, 260, 544 - 103, RGBA8(255,255,255,255));
+    vita2d_draw_line(960-260, 45, 960-260, 544, RGBA8(150,150,150,200));
 
     int num_buttons = static_cast<int>(buttons.size());
 
--- src/draw_templates.h
+++ src/draw_templates.h
@@ -8,9 +8,12 @@
 #include <vector>
 
 #define SUB_HEADER_BUTTONS      1
+#define BUTTONS_ONLY            2
 
 extern void drawTemplate(int type, std::string header, std::vector<Button> buttons, int cursor_pos);
 
 extern void subHeaderButtons(std::string header, std::vector<Button> buttons, int cursor_pos);
+
+extern void buttonsOnly(std::string header, std::vector<button> buttons, int cursor_pos);
 
 #endif
