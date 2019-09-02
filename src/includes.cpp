6b45ad63278492b343bff784bf53cb4daeb6224f
robDevs <robbdevv@gmail.com>
2019-09-02 15:33:09 -0600

    fix updater

--- src/includes.cpp
+++ src/includes.cpp
@@ -141,12 +141,12 @@ void check_update() {
     jansson_get_tag_from_release(curl_get_string("https://api.github.com/repos/robDevs/RepoHub/releases/latest"), &latest_tag, &url);
     std::string tag_string = std::to_string(latest_tag);
     tag_string.resize(4);
-    if(latest_tag > TAG) {
+    if(latest_tag > (float)TAG) {
         std::string message = "New version available: v";
         message += tag_string;
         message += "\nWould you like to download the update?";
 
-        if(draw_yes_no_message(message), "RepoHub.vpk") {
+        if(draw_yes_no_message(message)) {
             std::string file_name = "ux0:data/RepoHub/Downloads/RepoHub_";
             file_name += tag_string;
             file_name += ".vpk";
